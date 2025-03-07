d495b08f8b8d330614b4a8c70d8f40d2d460188e
Alani Rigotti <alani.ro02@gmail.com>
2025-03-07 09:10:40 -0300

    comandos da aula2

---
+++ testeGit.dart
@@ -0,0 +1,5 @@
+// git status mostra o como está os arquivos no repositorio, se tem algum push pendente, etc 
+// git log mostra detalhes das mudanças dos arquivos 
+// git diff --staged
+// git diff <hashDoCommit> <outroHash> afim de comparar as alterações no arquivo
+// git diff mostra as diferenças dos arquivos 
